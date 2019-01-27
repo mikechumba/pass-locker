d4a4fd68350b548438ee4ae97aa8bb60935a5089
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-28 00:16:27 +0300

    pass a test to check if their can be multiple user accounts and credentials

--- credentials_test.py
+++ credentials_test.py
@@ -24,6 +24,15 @@ class TestCredentials(unittest.TestCase):
       self.new_credentials.save_credentials()
       self.assertEqual(len(Credentials.credentials_list),1)
 
+   def test_save_multiple_contact(self):
+      '''
+      check if a user account can hold multiple credentials
+      '''
+      self.new_credentials.save_credentials()
+      test_credentials = Credentials("Twitter","michael","password") 
+      test_credentials.save_credentials()
+      self.assertEqual(len(Credentials.credentials_list),2)
+
 
 if __name__ == '__main__':
    unittest.main()
--- user_test.py
+++ user_test.py
@@ -23,6 +23,15 @@ class TestUser(unittest.TestCase):
       self.new_user.save_user()
       self.assertEqual(len(User.user_list),1)
 
+   def test_save_multiple_contact(self):
+      '''
+      check if we can hold multiple user accounts
+      '''
+      self.new_user.save_user()
+      test_user = User("andrew","password") 
+      test_user.save_user()
+      self.assertEqual(len(User.user_list),2)
+
 
 if __name__ == '__main__':
    unittest.main()
