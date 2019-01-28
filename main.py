d5c6b74daead1dee004605a1361ff0cf9f120b31
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-28 09:45:16 +0300

    create prompts for user sign up and log in

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from user_class import User
+from credentials import Credentials
 
 def create_User(username,password):
     '''
@@ -22,9 +23,44 @@ def del_user(user):
 #     return User.find_by_number(number)
 
 def log_in():
-   print("This is Password Locker. Enter your username to proceed")
 
-   user_name = input()
+   print("Do you have an account? Y/n")
 
-   if user_name == ''
+   has_account = input()
+
+   if has_account == "Y":
+      print("This is Password Locker. Enter your username to proceed:")
+
+      user_name = input()
+
+      if user_name in User.user_list:
+         print(f"Enter the password for {user_name}:")
+
+         pass_word = input()
+
+         print(User.user_list)
+      else:
+         print("Oops! Seems like you don't have an account. Press esc to go to the main page.")
+
+   else:
+      print("""
+      Create a new account
+      ________________________________________
+      """)
+      print("Enter a username:")
+
+      user_name = input()
+
+      if len(user_name) > 1:
+         print("Enter a password")
+
+         pass_word = input()
+
+      else:
+         print("Enter a valid username")
+   
+
+print(log_in())
+
+   
 
