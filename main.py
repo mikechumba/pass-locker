a0d71aa54608a371127df1407b716ad681c7c180
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-28 10:24:44 +0300

    define function to save new accounts

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from user_class import User
 from credentials import Credentials
+import getpass
 
-def create_User(username,password):
+def create_user(username,password):
     '''
     Function to create a new User
     '''
@@ -36,15 +37,13 @@ def log_in():
       if user_name in User.user_list:
          print(f"Enter the password for {user_name}:")
 
-         pass_word = input()
-
+         pass_word = getpass.getpass()
          print(User.user_list)
       else:
          print("Oops! Seems like you don't have an account. Press esc to go to the main page.")
 
    else:
-      print("""
-      Create a new account
+      print("""Create a new account
       ________________________________________
       """)
       print("Enter a username:")
@@ -54,10 +53,20 @@ def log_in():
       if len(user_name) > 1:
          print("Enter a password")
 
-         pass_word = input()
+         pass_word = getpass.getpass()
 
-      else:
-         print("Enter a valid username")
+         print("Confirm password")
+
+         pass_confirm = getpass.getpass()
+
+         if pass_word == pass_confirm:
+            create_user(user_name,pass_word)
+
+            print(f'''
+
+            *****************************
+
+            Welcome {user_name}''')
    
 
 print(log_in())
